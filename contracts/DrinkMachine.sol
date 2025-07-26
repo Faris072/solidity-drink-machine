ef129c9e5f2d74fd35a84673134c511486df48fe
Faris072 <farisbos.mfs@gmail.com>
2025-07-26 10:54:51 +0700

    init: DrinkMachine

---
+++ contracts/DrinkMachine.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract DrinkMachine {
+    struct ProductType {
+        string name;
+        uint price;
+        uint quantity;
+    }
+
+    address public owner;
+    mapping(address => uint) public transactions;
+    ProductType[5] internal products;
+    enum ProductEnum {Water, Tea, Juice, Milk, Coffee}
+
+    constructor() {
+        owner = msg.sender;
+        products[0] = ProductType("Water", 1 gwei, 100);
+        products[1] = ProductType("Tea", 2 gwei, 100);
+        products[2] = ProductType("Juice", 3 gwei, 100);
+        products[3] = ProductType("Milk", 4 gwei, 100);
+        products[4] = ProductType("Coffee", 5 gwei, 100);
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner can call this resource.");
+        _; // lanjut eksekusi fungsi jika kondisi terpenuhi
+    }
+
+    function addProduct(ProductEnum productEnum, uint quantity) public onlyOwner {
+        products[uint(productEnum)].quantity += quantity;
+    }
+
+    function getInfoProduct(ProductEnum productEnum) public view returns (ProductType memory){
+        return products[uint(productEnum)];
+    }
+
+    function buy(ProductEnum productEnum, uint qty) public payable {
+        uint index = uint(productEnum);
+        uint total = products[uint(productEnum)].price * qty;
+        require(index >= 0 || index < products.length, "Invalid product enum"); // jika kondisi terpenuhi maka akan di lanjutkan
+        require(products[index].quantity >= qty, "Product not enough."); // jika kondisi tidak terpenuhi maka akan ter trigger error
+        require(msg.value >= total, "Not enough ether");
+
+        products[index].quantity -= qty;
+        transactions[msg.sender] += msg.value;
+
+        // kirim saldo ke owner
+        payable(owner).transfer(total);
+    }
+
+    function withdrawAllFunds() public payable onlyOwner {
+        require(address(this).balance > 0, "No funds to withdraw.");
+        payable(owner).transfer(address(this).balance);
+    }
+}
